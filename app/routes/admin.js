46461cf1545054e142a8dbea2841423c00542be6
Shradha Pulla <pullashradha@gmail.com>
2016-08-25 18:30:46 -0700

    Create delete book functionality on admin, complete spec 2 & 3

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -9,6 +9,11 @@ export default Ember.Route.extend ({
       var newBook = this.store.createRecord("book", params);
       newBook.save();
       this.transitionTo("admin");
+    },
+    deleteBook(book) {
+      if(confirm("Are you sure you want to delete this book?")) {
+        book.destroyRecord(); //No need for .store Firebase reference when destroying
+      }
     }
   }
 });
