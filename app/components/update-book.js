0cfd58b8bbf667ccfe8b739badd2ce10c8fe5a04
Shradha Pulla <pullashradha@gmail.com>
2016-08-25 18:23:03 -0700

    Create update book functionality on admin, REMINDER: still include id values on update forms

--- app/components/update-book.js
+++ app/components/update-book.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend ({
+  updateBookForm: false,
+  actions: {
+    showUpdateBookForm() {
+      this.set("updateBookForm", true);
+    },
+    updateBook(book) {
+      book.save();
+      this.set("updateBookForm", false);
+    }
+  }
 });
